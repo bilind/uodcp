e8334043db4623ab65205a13f150d4e383699b34
Bilind <blndx@gmail.com>
2016-09-28 08:17:52 +0300

    Adding a theme to config file

--- conf.py
+++ conf.py
@@ -19,11 +19,9 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_rtd_theme
 
 html_theme = "sphinx_rtd_theme"
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = ["uodtemplates", ]
 
 # -- General configuration ------------------------------------------------
 
